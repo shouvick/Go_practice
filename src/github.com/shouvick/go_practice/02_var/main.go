12e1efa7d002ccb245d6bee3e9dcafb9779eed8a
shouvick <shouvick_shouvick@yahoo.com>
2019-09-13 12:49:08 +0600

    initial commit

---
+++ src/github.com/shouvick/go_practice/02_var/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//using var
+	var name = "shouvick"
+	var age = 25
+	const isActive = true
+	fmt.Println(name, age, isActive)
+	fmt.Printf("%T\n", age)
+}
